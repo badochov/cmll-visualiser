d147daf6075b426b94454bf4e4d726f407397753
Hubert Badocha <badocha.hubert@gmail.com>
2022-06-22 00:54:12 +0200

    Format

--- scripts/burger3.js
+++ scripts/burger3.js
@@ -1,118 +1,172 @@
-function burger(){
-	$( "#menu" ).slideToggle( "slow", function() {});
+function burger() {
+  $("#menu").slideToggle("slow", function () {});
 }
-$('#arrow').spectrum({
-	color:(localStorage.getItem("arr")!=null)?localStorage.getItem('arr'):'red',
-	change:(c)=>{arrowColor=c.toHexString();localStorage.setItem('arr',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#arrow").spectrum({
+  color:
+    localStorage.getItem("arr") != null ? localStorage.getItem("arr") : "red",
+  change: (c) => {
+    arrowColor = c.toHexString();
+    localStorage.setItem("arr", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#bg').spectrum({
-	color:(localStorage.getItem("bg")!=null)?localStorage.getItem('bg'):'#fafafa',
-	change:(c)=>{bgColor=c.toHexString();localStorage.setItem('bg',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#bg").spectrum({
+  color:
+    localStorage.getItem("bg") != null ? localStorage.getItem("bg") : "#fafafa",
+  change: (c) => {
+    bgColor = c.toHexString();
+    localStorage.setItem("bg", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#frame').spectrum({
-	color:(localStorage.getItem("frame")!=null)?localStorage.getItem('frame'):'black',
-	change:(c)=>{frameColor=c.toHexString();localStorage.setItem('frame',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#frame").spectrum({
+  color:
+    localStorage.getItem("frame") != null
+      ? localStorage.getItem("frame")
+      : "black",
+  change: (c) => {
+    frameColor = c.toHexString();
+    localStorage.setItem("frame", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#front').spectrum({
-	color:(localStorage.getItem("front")!=null)?localStorage.getItem('front'):'lime',
-	change:(c)=>{colors[1]=c.toHexString();localStorage.setItem('front',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#front").spectrum({
+  color:
+    localStorage.getItem("front") != null
+      ? localStorage.getItem("front")
+      : "lime",
+  change: (c) => {
+    colors[1] = c.toHexString();
+    localStorage.setItem("front", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#right').spectrum({
-	color:(localStorage.getItem("right")!=null)?localStorage.getItem('right'):'orange',
-	change:(c)=>{colors[2]=c.toHexString();localStorage.setItem('right',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#right").spectrum({
+  color:
+    localStorage.getItem("right") != null
+      ? localStorage.getItem("right")
+      : "orange",
+  change: (c) => {
+    colors[2] = c.toHexString();
+    localStorage.setItem("right", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#left').spectrum({
-	color:(localStorage.getItem("left")!=null)?localStorage.getItem('left'):'red',
-	change:(c)=>{colors[4]=c.toHexString();localStorage.setItem('left',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#left").spectrum({
+  color:
+    localStorage.getItem("left") != null ? localStorage.getItem("left") : "red",
+  change: (c) => {
+    colors[4] = c.toHexString();
+    localStorage.setItem("left", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#back').spectrum({
-	color:(localStorage.getItem("back")!=null)?localStorage.getItem('back'):'blue',
-	change:(c)=>{colors[3]=c.toHexString();localStorage.setItem('back',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#back").spectrum({
+  color:
+    localStorage.getItem("back") != null
+      ? localStorage.getItem("back")
+      : "blue",
+  change: (c) => {
+    colors[3] = c.toHexString();
+    localStorage.setItem("back", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#top').spectrum({
-	color:(localStorage.getItem("top")!=null)?localStorage.getItem('top'):'yellow',
-	change:(c)=>{colors[0]=c.toHexString();localStorage.setItem('top',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#top").spectrum({
+  color:
+    localStorage.getItem("top") != null
+      ? localStorage.getItem("top")
+      : "yellow",
+  change: (c) => {
+    colors[0] = c.toHexString();
+    localStorage.setItem("top", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-$('#flipped').spectrum({
-	color:(localStorage.getItem("flip")!=null)?localStorage.getItem('flip'):'lightblue',
-	change:(c)=>{color=c.toHexString();localStorage.setItem('flip',c.toHexString());generateState();},
-	showInput: true,
-	clickoutFiresChange: true,
-	preferredFormat: "hex"
+$("#flipped").spectrum({
+  color:
+    localStorage.getItem("flip") != null
+      ? localStorage.getItem("flip")
+      : "lightblue",
+  change: (c) => {
+    color = c.toHexString();
+    localStorage.setItem("flip", c.toHexString());
+    generateState();
+  },
+  showInput: true,
+  clickoutFiresChange: true,
+  preferredFormat: "hex",
 });
-function restoreDefaults(){
-	localStorage.clear();
-	$('#flipped').spectrum('set','lightblue');
-	color='lightblue';
-	$('#top').spectrum('set','yellow');
-	colors[0]='yellow';
-	$('#back').spectrum('set','blue');
-	colors[3]='blue';
-	$('#left').spectrum('set','red');
-	colors[4]='red';
-	$('#right').spectrum('set','orange');
-	colors[2]='orange';
-	$('#front').spectrum('set','lime');
-	colors[1]='lime';
-	$('#frame').spectrum('set','black');
-	frameColor='black';
-	$('#bg').spectrum('set','#fafafa');
-	bgColor='#fafafa';
-	$('#arrow').spectrum('set','red');
-	arrowColor='red';
-	generateState();
+function restoreDefaults() {
+  localStorage.clear();
+  $("#flipped").spectrum("set", "lightblue");
+  color = "lightblue";
+  $("#top").spectrum("set", "yellow");
+  colors[0] = "yellow";
+  $("#back").spectrum("set", "blue");
+  colors[3] = "blue";
+  $("#left").spectrum("set", "red");
+  colors[4] = "red";
+  $("#right").spectrum("set", "orange");
+  colors[2] = "orange";
+  $("#front").spectrum("set", "lime");
+  colors[1] = "lime";
+  $("#frame").spectrum("set", "black");
+  frameColor = "black";
+  $("#bg").spectrum("set", "#fafafa");
+  bgColor = "#fafafa";
+  $("#arrow").spectrum("set", "red");
+  arrowColor = "red";
+  generateState();
 }
-$('#eo').click(function() {
-        if($(this).is(':checked')){
-			drawEO=true;
-			$('.edge').show();
-		}
-        else{
-			drawEO=false;
-			$('.edge').hide();
-		}
+$("#eo").click(function () {
+  if ($(this).is(":checked")) {
+    drawEO = true;
+    $(".edge").show();
+  } else {
+    drawEO = false;
+    $(".edge").hide();
+  }
 });
-$('#ep').click(function() {
-        if($(this).is(':checked')){
-			drawEP=true;
-			$('.arrow').show();
-		}
-        else{
-			drawEP=false;
-			$('.arrow').hide();
-		}
+$("#ep").click(function () {
+  if ($(this).is(":checked")) {
+    drawEP = true;
+    $(".arrow").show();
+  } else {
+    drawEP = false;
+    $(".arrow").hide();
+  }
 });
-$('#cll').click(function() {
-        if($(this).is(':checked')){
-			drawCMLL=true;
-			$('.face').show();
-		}
-        else{
-			drawCMLL=false;
-			$('.face').hide();
-		}
+$("#cll").click(function () {
+  if ($(this).is(":checked")) {
+    drawCMLL = true;
+    $(".face").show();
+  } else {
+    drawCMLL = false;
+    $(".face").hide();
+  }
 });
--- scripts/moves.js
+++ scripts/moves.js
@@ -1,183 +1,181 @@
-function R(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[1];
-		cube[1]=cube[4];
-		cube[4]=cube[9];
-		cube[9]=cube[5];
-		cube[5]=pom;
-	}
+function R(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[1];
+    cube[1] = cube[4];
+    cube[4] = cube[9];
+    cube[9] = cube[5];
+    cube[5] = pom;
+  }
 }
-function U(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[1];
-		cube[1]=cube[2];
-		cube[2]=cube[3];
-		cube[3]=cube[0];
-		cube[0]=pom;
-	}
+function U(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[1];
+    cube[1] = cube[2];
+    cube[2] = cube[3];
+    cube[3] = cube[0];
+    cube[0] = pom;
+  }
 }
-function F(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[0];
-		cube[0]=cube[7];
-		cube[7]=cube[8];
-		cube[8]=cube[4];
-		cube[4]=pom;
-		
-		cube[0][1]++;
-	    cube[4][1]++;
-	    cube[8][1]++;
-	    cube[7][1]++;
-	}
+function F(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[0];
+    cube[0] = cube[7];
+    cube[7] = cube[8];
+    cube[8] = cube[4];
+    cube[4] = pom;
+
+    cube[0][1]++;
+    cube[4][1]++;
+    cube[8][1]++;
+    cube[7][1]++;
+  }
 }
-function L(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[3];
-		cube[3]=cube[6];
-		cube[6]=cube[11];
-		cube[11]=cube[7];
-		cube[7]=pom;
-	}
+function L(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[3];
+    cube[3] = cube[6];
+    cube[6] = cube[11];
+    cube[11] = cube[7];
+    cube[7] = pom;
+  }
 }
-function D(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[8];
-		cube[8]=cube[11];
-		cube[11]=cube[10];
-		cube[10]=cube[9];
-		cube[9]=pom;
-	}
+function D(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[8];
+    cube[8] = cube[11];
+    cube[11] = cube[10];
+    cube[10] = cube[9];
+    cube[9] = pom;
+  }
 }
-function B(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[2];
-		cube[2]=cube[5];
-		cube[5]=cube[10];
-		cube[10]=cube[6];
-		cube[6]=pom;
-		
-		cube[2][1]++;
-		cube[5][1]++;
-		cube[10][1]++;
-		cube[6][1]++;
-	}
+function B(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[2];
+    cube[2] = cube[5];
+    cube[5] = cube[10];
+    cube[10] = cube[6];
+    cube[6] = pom;
+
+    cube[2][1]++;
+    cube[5][1]++;
+    cube[10][1]++;
+    cube[6][1]++;
+  }
 }
-function M(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[2];
-		cube[2]=cube[10];
-		cube[10]=cube[8];
-		cube[8]=cube[0];
-		cube[0]=pom;
-		
-		cube[2][1]++;
-		cube[8][1]++;
-		cube[10][1]++;
-		cube[0][1]++;
-	}
+function M(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[2];
+    cube[2] = cube[10];
+    cube[10] = cube[8];
+    cube[8] = cube[0];
+    cube[0] = pom;
+
+    cube[2][1]++;
+    cube[8][1]++;
+    cube[10][1]++;
+    cube[0][1]++;
+  }
 }
-function S(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[1];
-		cube[1]=cube[9];
-		cube[9]=cube[11];
-		cube[11]=cube[3];
-		cube[3]=pom;
-		
-		cube[1][1]++;
-	    cube[9][1]++;
-	    cube[3][1]++;
-	    cube[11][1]++;
-	}
+function S(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[1];
+    cube[1] = cube[9];
+    cube[9] = cube[11];
+    cube[11] = cube[3];
+    cube[3] = pom;
+
+    cube[1][1]++;
+    cube[9][1]++;
+    cube[3][1]++;
+    cube[11][1]++;
+  }
 }
-function E(n){
-	for(let i=0;i<n;i++){
-		let pom=cube[7];
-		cube[7]=cube[6];
-		cube[6]=cube[5];
-		cube[5]=cube[4];
-		cube[4]=pom;
-		
-		cube[6][1]++;
-	    cube[4][1]++;
-	    cube[5][1]++;
-	    cube[7][1]++;
-	}
+function E(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = cube[7];
+    cube[7] = cube[6];
+    cube[6] = cube[5];
+    cube[5] = cube[4];
+    cube[4] = pom;
+
+    cube[6][1]++;
+    cube[4][1]++;
+    cube[5][1]++;
+    cube[7][1]++;
+  }
 }
 
+function Uc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[0];
+    corn[0] = corn[1];
+    corn[1] = corn[2];
+    corn[2] = corn[3];
+    corn[3] = pom;
+  }
+}
+function Dc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[4];
+    corn[4] = corn[5];
+    corn[5] = corn[6];
+    corn[6] = corn[7];
+    corn[7] = pom;
+  }
+}
+function Rc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[0];
+    corn[0] = corn[7];
+    corn[7] = corn[6];
+    corn[6] = corn[1];
+    corn[1] = pom;
 
+    corn[0][1]--;
+    corn[7][1]++;
+    corn[6][1]--;
+    corn[1][1]++;
+  }
+}
+function Lc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[2];
+    corn[2] = corn[5];
+    corn[5] = corn[4];
+    corn[4] = corn[3];
+    corn[3] = pom;
 
-function Uc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[0];
-		corn[0]=corn[1];
-		corn[1]=corn[2];
-		corn[2]=corn[3];
-		corn[3]=pom;
-	}
+    corn[2][1]--;
+    corn[5][1]++;
+    corn[4][1]--;
+    corn[3][1]++;
+  }
 }
-function Dc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[4];
-		corn[4]=corn[5];
-		corn[5]=corn[6];
-		corn[6]=corn[7];
-		corn[7]=pom;
-	}
+function Fc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[3];
+    corn[3] = corn[4];
+    corn[4] = corn[7];
+    corn[7] = corn[0];
+    corn[0] = pom;
+
+    corn[3][1]--;
+    corn[4][1]++;
+    corn[7][1]--;
+    corn[0][1]++;
+  }
 }
-function Rc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[0];
-		corn[0]=corn[7];
-		corn[7]=corn[6];
-		corn[6]=corn[1];
-		corn[1]=pom;
-		
-		corn[0][1]--;
-		corn[7][1]++;
-		corn[6][1]--;
-		corn[1][1]++;
-	}
-}
-function Lc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[2];
-		corn[2]=corn[5];
-		corn[5]=corn[4];
-		corn[4]=corn[3];
-		corn[3]=pom;
-		
-		corn[2][1]--;
-		corn[5][1]++;
-		corn[4][1]--;
-		corn[3][1]++;
-	}
-}
-function Fc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[3];
-		corn[3]=corn[4];
-		corn[4]=corn[7];
-		corn[7]=corn[0];
-		corn[0]=pom;
-		
-		corn[3][1]--;
-		corn[4][1]++;
-		corn[7][1]--;
-		corn[0][1]++;
-	}
-}
-function Bc(n){
-	for(let i=0;i<n;i++){
-		let pom=corn[1];
-		corn[1]=corn[6];
-		corn[6]=corn[5];
-		corn[5]=corn[2];
-		corn[2]=pom;
-		
-		corn[1][1]--;
-		corn[6][1]++;
-		corn[5][1]--;
-		corn[2][1]++;
-	}
+function Bc(n) {
+  for (let i = 0; i < n; i++) {
+    let pom = corn[1];
+    corn[1] = corn[6];
+    corn[6] = corn[5];
+    corn[5] = corn[2];
+    corn[2] = pom;
+
+    corn[1][1]--;
+    corn[6][1]++;
+    corn[5][1]--;
+    corn[2][1]++;
+  }
 }
